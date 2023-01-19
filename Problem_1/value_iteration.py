43760fe8c35809c876e45c9617e8284e55f8d1a8
unknown <ricardopaschoeto@gmail.com>
2023-01-19 20:27:30 -0300

    Adjust in heatmap - added policy imp - create path

--- Problem_1/value_iteration.py
+++ Problem_1/value_iteration.py
@@ -3,6 +3,7 @@ import os, sys, pdb, math, pickle, time
 import matplotlib
 import tensorflow as tf, numpy as np, matplotlib.pyplot as plt
 import seaborn as sb
+from matplotlib.patches import Rectangle
 
 from utils import generate_problem, visualize_value_function
 
@@ -10,13 +11,15 @@ from utils import generate_problem, visualize_value_function
 def value_iteration(problem, reward, terminal_mask, gam):
     Ts = problem["Ts"]
     sdim, adim = Ts[0].shape[-1], len(Ts)  # state and action dimension
-    V = tf.zeros([sdim])
+    V = tf.Variable(tf.zeros([sdim]))
+
+    policy = np.zeros((sdim))
+    for pos, s in enumerate(problem["idx2pos"]):
+        aS = actions_space(s, adim, problem["m"], problem["n"])
+        policy[pos] = np.random.choice(aS)
 
     assert terminal_mask.ndim == 1 and reward.ndim == 2
-    
-    m = problem["m"]
-    n = problem["n"]
-    V = tf.Variable(V)
+
     # perform value iteration
     for ii in range(1000):
         ######### Your code starts here #########
@@ -31,12 +34,19 @@ def value_iteration(problem, reward, terminal_mask, gam):
 
         # compute the next value function estimate for the iteration
         # compute err = tf.linalg.norm(V_new - V_prev) as a breaking condition
-
-        ######### Your code ends here ###########
         err = 0.
         for pos, s in enumerate(problem["idx2pos"]):
-            aS = actions_space(s, adim, m, n)
-            best = bellman_optimality_update(problem, V, s, pos, aS, adim, gam, reward, terminal_mask)
+            aS = actions_space(s, adim,  problem["m"],  problem["n"])
+            v = bellman_optimality_update(problem, V, s, pos, aS, adim, gam, reward)
+            if terminal_mask[pos] != 1:
+                best = max(v)
+                policy[pos] = np.argmax(v)
+            else:
+                temp = []
+                for u in aS:
+                    temp.append(reward[pos, u])
+                best = max(temp)
+
             err = max(err, tf.linalg.norm(V[pos] - best))
             V[pos].assign(best)
 
@@ -47,30 +57,28 @@ def value_iteration(problem, reward, terminal_mask, gam):
             print(ii)
             print(tf.get_static_value(err))
             break
+        ######### Your code ends here ###########'
+    return V, policy
 
-    return V
-
-def bellman_optimality_update(problem, V, s, s_pos, aS, adim, gamma, reward, terminal_mask):
-    """Mutate ``V`` according to the Bellman optimality update equation."""
+## Compute the bellman equation presented in Problem 01
+def bellman_optimality_update(problem, V, s, s_pos, aS, adim, gamma, reward):
     values = np.zeros((adim))
-    if terminal_mask[s_pos] != 1:
-        for u in aS:
-            for u_iter in aS:
-                if u == u_iter:
-                    s_next = next_state(problem["m"], problem["n"], s, u)
-                else:
-                    s_next = next_state(problem["m"], problem["n"], s, u_iter)
+    for u in aS:
+        for u_iter in aS:
+            if u == u_iter:
+                s_next = next_state(problem["m"], problem["n"], s, u)
+            else:
+                s_next = next_state(problem["m"], problem["n"], s, u_iter)
 
-                idx = problem["pos2idx"][s_next[0], s_next[1]]
-                p = problem["Ts"][u][problem["pos2idx"][s[0], s[1]], idx]
-                values[u] += gamma * p * V[idx]
+            idx_next = problem["pos2idx"][s_next[0], s_next[1]]
+            p = problem["Ts"][u][s_pos, idx_next]
+            values[u] += tf.convert_to_tensor(gamma * p * V[idx_next])
 
-            best = np.max(reward[s_pos, u] + values[u])
-    else:
-        best = reward[s_pos, 0]
+        values[u] += reward[s_pos, u]
 
-    return best
+    return values
 
+## Compute the permitted action for each state
 def actions_space(state, adim, m, n):
     act = []
     for u in range(adim):
@@ -78,7 +86,33 @@ def actions_space(state, adim, m, n):
         if state[0] != s_next[0] or state[1] != s_next[1]:
             act.append(u)
     return act
-    
+
+## Function to plot heatmap from scratch
+def plot_heatmap(problem, data, m, n, ant):
+    dt = np.reshape(np.array(data), (m,n)).T
+    ax = sb.heatmap(np.round(dt, 3), annot=ant)
+    ax.invert_yaxis()
+    p = path(problem, data, [0, 0], [19, 9], m, n)
+    for t in p:
+        ax.add_patch(Rectangle(t, 1, 1, fill=False, edgecolor='blue', lw=3))
+    plt.show()
+
+## Function to simulate the MDP starting from x = (0, 0) over N = 100 time steps
+def path(problem, policy, s_start, s_goal, m, n):
+    p = []
+    s = s_start
+    N = 100
+    p.append(s_start)
+    iteration = 0
+    while iteration <= N:
+        if s[0] == s_goal[0] and s[1] == s_goal[1]:
+            break
+        s = next_state(m, n, s, policy[problem["pos2idx"][s[0], s[1]]])
+        p.append(s)
+        iteration += 1
+    return p
+
+## Compute the next state s next given an actual state and ction   
 def next_state(m, n, s, u):
     xclip = lambda x: min(max(0, x), m - 1)
     yclip = lambda y: min(max(0, y), n - 1)
@@ -91,12 +125,6 @@ def next_state(m, n, s, u):
         return np.array([xclip(s[0] - 1), yclip(s[1] + 0)])
     else:
         return np.array([xclip(s[0] + 0), yclip(s[1] - 1)])
-
-def plot_heatmap(data, ant):
-    m = np.array(data)
-    ax = sb.heatmap(np.round(m, 3), annot=ant)
-    ax.invert_yaxis()
-    plt.show()
 
 # value iteration ##############################################################
 def main():
@@ -116,14 +144,14 @@ def main():
     reward = tf.convert_to_tensor(reward, dtype=tf.float32)
 
     gam = 0.95
-    V_opt = value_iteration(problem, reward, terminal_mask, gam)
+    V_opt, policy = value_iteration(problem, reward, terminal_mask, gam)
 
-    #plt.figure(213)
-    #visualize_value_function(np.array(V_opt).reshape((n, n)))
-    #plt.title("value iteration")
-    #plt.show()
-    plot_heatmap(V_opt, False)
-
+    # plt.figure(213)
+    # visualize_value_function(np.array(V_opt).reshape((n, n)))
+    # plt.title("value iteration")
+    # plt.show()
+    plot_heatmap(problem, V_opt, n, n, False)
+    plot_heatmap(problem, policy, n, n, True)
 
 if __name__ == "__main__":
     main()
